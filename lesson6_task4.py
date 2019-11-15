434c25656a53ba0dcc6be56253fbb8ffdc56511f
Kaurov2000 <56529033+Kaurov2000@users.noreply.github.com>
2019-11-15 12:41:46 +0500

    Add files via upload

--- lesson6_task4.py
+++ lesson6_task4.py
@@ -16,14 +16,14 @@ class Car:
         print(f"{self.name} turned {direction}")
 
     def show_speed(self):
-        print(f"{self.name} moves with speed {speed}")
+        print(f"{self.name} moves with speed {self.speed}")
 
 class TownCar(Car):
     max_speed = 60
 
     def show_speed(self):
-        print(f"{self.name} moves with speed {speed}")
-        if self.speed > max_speed:
+        print(f"{self.name} moves with speed {self.speed}")
+        if self.speed > self.max_speed:
             print(f"{self.name} moves too fast for a towncar")
 
 class SportCar(Car):
@@ -33,11 +33,17 @@ class WorkCar(Car):
     max_speed = 40
 
     def show_speed(self):
-        print(f"{self.name} moves with speed {speed}")
-        if self.speed > max_speed:
+        print(f"{self.name} moves with speed {self.speed}")
+        if self.speed > self.max_speed:
             print(f"{self.name} moves too fast for a workcar")
 
 class PoliceCar(Car):
     max_speed = 100
     is_police = True
 
+car1 = TownCar()
+car1.name = 'Sandero'
+car1.color = 'red'
+
+car1.go(80)
+car1.show_speed()
